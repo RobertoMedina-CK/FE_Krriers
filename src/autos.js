a2a124a67f949ad6252518630489b729e10af4b3
RobertoMedina-CK <81485892+RobertoMedina-CK@users.noreply.github.com>
2024-07-26 13:42:06 -0500

    Update autos.js

--- src/autos.js
+++ src/autos.js
@@ -37,7 +37,7 @@ function Autos() {
       let modeloL = modelo.toLowerCase();
       
      
-    Axios.post("http://localhost:3001/autos",{
+    Axios.post(`https://krriers.moveurads.com/autos`,{
 
       marca:marca,
       modelo:modelo,
@@ -66,7 +66,7 @@ function Autos() {
   }
 
   const update = ()=> {
-    Axios.put("http://localhost:3001/autos",{
+    Axios.put(`https://krriers.moveurads.com/autos`,{
 
       id:id,
       marca:marca,
@@ -108,7 +108,7 @@ function Autos() {
       confirmButtonText: "Si, eliminarlo!"
     }).then((result) => {
       if (result.isConfirmed) {
-        Axios.delete(`http://localhost:3001/autos/${val.id}`).then(()=>{
+        Axios.delete(`https://krriers.moveurads.com/autos/${val.id}`).then(()=>{
           getAutos();
           limpiarCampos();
           Swal.fire({
@@ -157,7 +157,7 @@ const editarAuto = (val)=>{
 }
 
   const getAutos = ()=> {
-    Axios.get("http://localhost:3001/autos").then((response)=>{
+    Axios.get(`https://krriers.moveurads.com/autos`).then((response)=>{
         setAutos(response.data);
         setFilteredAutos(response.data);
     });
