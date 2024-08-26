1045896b16a4aeae2b80fa5d87bb57591549f915
RobertoMedina-CK <81485892+RobertoMedina-CK@users.noreply.github.com>
2024-08-25 19:09:44 -0500

    Update authService.js

--- src/authService.js
+++ src/authService.js
@@ -1,1 +1,18 @@
+const getCurrentUser = () => {
+    return JSON.parse(localStorage.getItem('user'))
+}
+
+const login = (username, password) => {
+    localStorage.setItem('user', JSON.stringify({user: username, password: password}))
+}
+
+const logout = () => {
+    localStorage.removeItem('user');
+   }
+
+export {
+    getCurrentUser,
+    login,
+    logout
+}
 
