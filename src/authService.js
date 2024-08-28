3061b7e0e0fb49e8fa07d3f7bd67509fb6a13b82
RobertoMedina-CK <81485892+RobertoMedina-CK@users.noreply.github.com>
2024-08-28 15:10:21 -0500

    Update authService.js

--- src/authService.js
+++ src/authService.js
@@ -6,13 +6,20 @@ const login = (username, password) => {
     localStorage.setItem('user', JSON.stringify({user: username, password: password}))
 }
 
-const logout = () => {
-    localStorage.removeItem('user');
-   }
+const logout = () => 
+    {
+        localStorage.removeItem('user');
+        if (localStorage.getItem('user')) { 
+            //check something in local storage so you can know
+            // if you should reload or not 
+    
+            window.location.reload();
+        }
+        return 'you were logout';
+    };
 
 export {
     getCurrentUser,
     login,
     logout
 }
-
